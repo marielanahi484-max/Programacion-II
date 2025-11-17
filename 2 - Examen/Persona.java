72f12abaa7e313763c8b037723c38cd530085872
marielanahi484-max <marielanahi484@gmail.com>
2025-11-17 09:29:45 -0400

    Create Persona.java

---
+++ 2 - Examen/Persona.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class Persona {
+	String nombre;
+	int edad;
+	float pesoPersona;
+
+public Persona(String nombre, int edad, float peso) {
+	this.nombre = nombre;
+	this.edad = edad;
+	this.pesoPersona = peso;
+	}
+}
