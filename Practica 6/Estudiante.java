03a63488ba128bad78cc01f09efe42f1d4516320
marielanahi484-max <marielanahi484@gmail.com>
2025-12-03 16:29:27 -0400

    Update Estudiante.java

--- Practica 6/Estudiante.java
+++ Practica 6/Estudiante.java
@@ -1,23 +1,14 @@
-package modelo;
-
 public class Estudiante {
-    private String codigo;
-    private String nombre;
-
-    public Estudiante() {}
+    public String codigo;
+    public String nombre;
 
     public Estudiante(String codigo, String nombre) {
         this.codigo = codigo;
         this.nombre = nombre;
     }
 
-    public String getCodigo() { return codigo; }
-    public void setCodigo(String codigo) { this.codigo = codigo; }
-
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public void mostrarInfo() {
-        System.out.println("Estudiante: " + nombre + " | Código: " + codigo);
+    @Override
+    public String toString() {
+        return nombre + " [" + codigo + "]";
     }
 }
