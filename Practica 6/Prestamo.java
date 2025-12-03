4632ea7d782d75d060d9fc962082e795f799a6f8
marielanahi484-max <marielanahi484@gmail.com>
2025-12-03 16:32:32 -0400

    Update Prestamo.java

--- Practica 6/Prestamo.java
+++ Practica 6/Prestamo.java
@@ -1,36 +1,11 @@
-package modelo;
-
 public class Prestamo {
-    private String fechaPrestamo;
-    private String fechaDevolucion;
-    private Estudiante estudiante;
-    private Libro libro;
+    public String fechaPrestamo = "29/11/2025";
+    public String fechaDevolucion = "13/12/2025";
+    public Estudiante estudiante;
+    public Libro libro;
 
-    public Prestamo() {}
-
-    public Prestamo(String fechaPrestamo, String fechaDevolucion, Estudiante estudiante, Libro libro) {
-        this.fechaPrestamo = fechaPrestamo;
-        this.fechaDevolucion = fechaDevolucion;
+    public Prestamo(Estudiante estudiante, Libro libro) {
         this.estudiante = estudiante;
         this.libro = libro;
-    }
-
-    public String getFechaPrestamo() { return fechaPrestamo; }
-    public void setFechaPrestamo(String fechaPrestamo) { this.fechaPrestamo = fechaPrestamo; }
-
-    public String getFechaDevolucion() { return fechaDevolucion; }
-    public void setFechaDevolucion(String fechaDevolucion) { this.fechaDevolucion = fechaDevolucion; }
-
-    public Estudiante getEstudiante() { return estudiante; }
-    public void setEstudiante(Estudiante estudiante) { this.estudiante = estudiante; }
-
-    public Libro getLibro() { return libro; }
-    public void setLibro(Libro libro) { this.libro = libro; }
-
-    public void mostrarInfo() {
-        System.out.println("Libro: " + libro.getTitulo() +
-                " | Estudiante: " + estudiante.getNombre() +
-                " | Prestamo: " + fechaPrestamo +
-                " | Devolución: " + fechaDevolucion);
     }
 }
